63c4dac17a3316b04cf94d1d49ca77a0aa7598b9
Johnny Nigh <johnny.a.nigh@gmail.com>
2021-11-15 16:03:25 -0500

    com

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  name = var.name
 
   ami                    = "ami-02e136e904f3da870"
   instance_type          = "t2.micro"
@@ -14,8 +14,4 @@ module "ec2_instance" {
   monitoring             = false
   vpc_security_group_ids = ["sg-fe1ca8db", "sg-0e9b771d788ff0d80", "sg-0d0400719b48d7725"]
   subnet_id              = "subnet-0b5dbb6b1fdcf015c"
-
-  tags = {
-    Name   = "tf1"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   type        = string
   description = "The region"
 }
+
+variable "name" {
+  type        = string
+  description = "Name of the EC2 instance"
+}
