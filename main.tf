bdf40e7f072fe7a40dae5636e25ce88a2047b3b4
Johnny Nigh <johnny.a.nigh@gmail.com>
2021-11-15 15:48:46 -0500

    Add EC2

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region  = var.region
 }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-02e136e904f3da870"
+  instance_type          = "t2.micro"
+  key_name               = "myWindows"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-fe1ca8db", "sg-0e9b771d788ff0d80", "sg-0d0400719b48d7725"]
+  subnet_id              = "subnet-0b5dbb6b1fdcf015c"
+
+  tags = {
+    Name   = "tf1"
+  }
+}
